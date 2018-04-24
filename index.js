d49d23814a0f64a4b7358b9d6320df97ecd628d1
Handrew <andrew.a.han@gmail.com>
2018-04-23 21:29:13 -0400

    created things.js to hold all the path/routes for index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+var things = require('./things.js');
+
+app.get('/hello', function(req, res) {
     res.send("hello world!");
+});
+
+app.post('/hello', function(req, res) {
+    res.send("You just called the post method at '/helo'!\m");
+});
+
+app.all('/test', function(req, res) {
+    res.send("HTTP method doesn't have any effect on this route!");
 });
 
 app.listen(3000);
